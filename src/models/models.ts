246283e3cc482ddc1f198136beadcb8ac6435652
gabriel <15620759+genrique98@users.noreply.github.com>
2021-10-07 20:58:04 -0700

    feat: added book, author, and publisher models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,24 +1,37 @@
-export type Book = {
+export type Book_model = {
     id: string,
     title: string,
-    author: string,
-    category: string
-}
+    category: string,
+    author_id: number,
+    publisher_id: number
+};
 
-export type User = {
+export type User_model = {
     id: string,
     name: string,
     email: string,
     username: string,
     password: string
-}
+};
+
+export type Author_model = {
+    id: string,
+    name: string,
+    lastName: string,
+    bio: string
+};
+  
+export type Publisher_model = {
+    id: string,
+    name: string
+};
 
 export type AuthPayload = {
     token: string,
-    user: User
-}
+    user: User_model
+};
 
 export type Context = {
     id?: string,
     req: string
-}
+};
